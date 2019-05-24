c06eee3a4ee2e7e9c3d6651f6a2fadacd7169038
Zuzanna Pusiewicz <zuzpusie@student.pg.gda.pl>
2019-05-24 13:30:11 +0200

    add authentication

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,5 +18,13 @@
 //= require_tree .
 
 $(document).on('turbolinks:load', function() {
-    $('.ui.dropdown').dropdown()
+    $('.ui.dropdown').dropdown();
+    $('.message .close')
+    .on('click', function() {
+    $(this)
+      .closest('.message')
+      .transition('fade')
+    ;
+  })
+;
 })
